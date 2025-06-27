46ba525605291e09c1b4ab991292f3a8159f141d
Hadeel <Hadaust213@outlook.com>
2025-06-27 18:01:23 +1000

    update from main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { ThemeProvider } from './context/ThemeContext'; // تأكدي من المسار
+import { ThemeProvider } from './context/ThemeContext'; 
 
-import './index.css'; // لازم تكون Tailwind مفعلة
+import './index.css'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
